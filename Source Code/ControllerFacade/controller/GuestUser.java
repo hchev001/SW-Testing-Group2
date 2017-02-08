8d67fff641cba187559bc7356c019235c0b2e647
Hamilton Chevez <Hamilton Chevez>
2017-02-07 23:53:56 -0500

    created the facade project folder

---
+++ Source Code/ControllerFacade/controller/GuestUser.java
@@ -0,0 +1,19 @@
+
+package server.controller;
+/*
+ * This is an entity class to create a guest user object who is a parking user
+ */
+/**
+ * 
+ * @author Team5
+ */
+public class GuestUser extends ParkingUser
+{
+
+    @Override
+    public String toString()
+    {
+        return "Guest";
+    }
+    
+}
