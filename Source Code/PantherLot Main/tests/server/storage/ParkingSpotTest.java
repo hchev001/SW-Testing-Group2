74f43a3a745e580cc0809eb5463f1e5aa413941a
Chris Hidalgo <chidalgo088@gmail.com>
2017-02-12 13:38:42 -0500

    Update ParkingSpotTest.java
    Some of the unit tests that i have created, so far they pass. Dane, feel free to change or add anything if you see fit to do so.

--- Source Code/PantherLot Main/tests/server/storage/ParkingSpotTest.java
+++ Source Code/PantherLot Main/tests/server/storage/ParkingSpotTest.java
@@ -2,61 +2,145 @@ package server.storage;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import server.controller.*;
+
+import org.junit.Before;
+
+
 public class ParkingSpotTest {
+
+	// Variables used throughout the tests
+	ParkingSpot testSpot;
 	
-	/* Setups and Before
-	 * -------------------
-	 * Place anything that needs to be run before 
-	 * the tests here. You can add the multiple @Before
-	 * methods here in this format:
-	 * 
-	 * @Before
-	 * setUpForxxTests() {
-	 * }
-	 * 
-	 * Where xx corresponds to the tests using that 
-	 * numbering format starting at 01.
-	 ===================================================*/
+	ParkingUser person01;
+	ParkingUser person02;
+	ParkingUser person03;
+	ParkingUser person04;
+	ParkingUser person05;
+	
+
+	
 	@Before
-	public void setUpFor01Tests() {
+	/*
+	 * Set up for the tests to be executed
+	 */
+	public void setUp(){
 		
+		testSpot = new ParkingSpot(300 , "Guest" , 3 , "300" , "South");
+		person01 = new StudentUser("Carl" , "7654321");
+		person02 = new FacultyUser("John" , "1234567");
+		person03 = new GuestUser();
+		person04 = new HandicappedUser();
 	}
 	
-	
-	/* Tests
-	 * ------------------
-	 * Runs the tests here. Name them what you want but
-	 * use this format:
+	@Test
+	/*
+	 * This is testing the assigning and fetching of a Student User
+	 */
+	public void test_AssignParkingSpot_01() {
+		
+		testSpot.assignParkingSpot(person01);
+
+		ParkingUser test = testSpot.getUser();
+		
+		assertEquals(person01 , test);
+		
+	}
+
+	@Test
+	/*
+	 * This is testing the assigning and fetching of a Faculty User
 	 * 
-	 * nameOfTestingMethodxx() {
-	 * }
+	 */
+	public void test_AssignParkingSpot_02() {
+		
+		testSpot.assignParkingSpot(person02);
+
+		ParkingUser test = testSpot.getUser();
+		
+		assertEquals(person02 , test);
+		
+	}
+	
+	@Test
+	/*
+	 * This is testing the assigning and fetching of a Guest User
 	 * 
-	 * Where xx corresponds to the setup numbering 
-	 * format starting at 01.
-	 ===================================================*/
-	
-	//Tests if the getParkingSpot method returns the parkingSpotNumber
-	//provided to it
-	@Test
-	public void getParkingSpotTest01() {
+	 */
+	public void test_AssignParkingSpot_03() {
+		
+		testSpot.assignParkingSpot(person03);
+
+		ParkingUser test = testSpot.getUser();
+		
+		assertEquals(person03 , test);
 		
 	}
 	
-	//Tests to see the getParkingSpot method returns an empty string
-	//if an empty string is provided as the parkingSpotNumber
-	//NOTE: This test may not be necessary!
 	@Test
-	public void getParkingSpotEmptyStringTest01() {
+	/*
+	 * This is testing the assigning and fetching of a Student User
+	 * 
+	 */
+	public void test_AssignParkingSpot_04() {
+		
+		testSpot.assignParkingSpot(person04);
+
+		ParkingUser test = testSpot.getUser();
+		
+		assertEquals(person04 , test);
 		
 	}
 	
-	//Tests to see the getParkingSpot method throws an error if the 
-	//parkingNumber is provided as an integer instead of a string.
 	@Test
-	public void getParkingSpotIntegerTest01() {
+	/*
+	 * This is for the testing for the removal of a parked user from the spot
+	 * 
+	 */
+	public void test_RemoveUser() {
 		
+		testSpot.removeAssignedUser();;
+		
+		ParkingUser test = testSpot.getUser();
+		
+		assertEquals(null , test);
+		
+	}
+	
+	
+	@Test
+	/*
+	 * Testing for the retrieval of the Parking Spot
+	 */
+	public void test_getParkingSpot_int() {
+		
+		int test = testSpot.getParkingSpot();
+		
+		assertEquals(300 , test);
+		
+	}
+	
+	@Test
+	/*
+	 * Testing for the retrieval of the Parking Number
+	 */
+	public void test_getParkingNUmber_String() {
+		
+		String test = testSpot.getParkingNumber();
+		
+		assertEquals(300 , test);
+	}
+	
+	@Test
+	/*
+	 * Testing for the retrieval of the Parking Number
+	 */
+	public void test_getFloor() {
+		
+		int test = testSpot.getFloor();
+		
+		assertEquals(3 , test);
 	}
 }
