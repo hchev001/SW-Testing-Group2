367cd75ba3b89a4cbe10774896b2e36166fc258e
Hamilton Chevez <hchev001@fiu.edu>
2017-02-23 01:10:37 -0500

    created three test cases for facade.displayParkingSpotAssigned, that is tested is the label created in the method that is set in the display.

---
+++ Source Code/PantherLot Main/tests/server/controller/displayParkingSpotAssigned_spotPickedNone_spotNumberAssignedNone.java
@@ -0,0 +1,47 @@
+package server.controller;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import client.maindisplay.SpotNumberDisplay;
+import server.storage.ParkingSpot;
+
+public class displayParkingSpotAssigned_spotPickedNone_spotNumberAssignedNone {
+	
+	ControllerFacade facade;
+	SpotNumberDisplay spotDisplay;
+	ParkingSpot spot;
+	EntranceDisplayController displayController;
+	@Before
+	public void setUp() throws Exception {
+		facade = new ControllerFacade();
+		facade.createEntranceDisplayController(facade);
+		
+		spotDisplay = mock( SpotNumberDisplay.class);
+		doReturn(true).when(spotDisplay).runDisplay(null);
+		
+		spot = mock(ParkingSpot.class);
+		when(spot.getParkingNumber()).thenReturn("");
+		
+		// inject dependencies into facade entranceDisplayController
+		facade.getEntranceDisplayController().setsDisp(spotDisplay);
+		facade.getEntranceDisplayController().setSpot(spot);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDisplayParkingSpotAssigned() {
+		assertEquals(facade.displayParkingSpotAssigned(), "Your spot number is ");
+	}
+
+
+}
