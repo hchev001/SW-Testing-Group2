8ba12b90d90194d892f65caaf2c86a7bcda1f5e3
Hamilton Chevez <hchev001@fiu.edu>
2017-04-20 12:06:41 -0400

    changed the names of the tests

--- Source Code/PantherLot Main/tests/server/controller/SST024_EntranceDisplayController_DisplayGettersSettersTest.java
+++ Source Code/PantherLot Main/tests/server/controller/SST024_EntranceDisplayController_DisplayGettersSettersTest.java
@@ -11,13 +11,26 @@ import client.maindisplay.DisplayDirections;
 import client.maindisplay.ParkingNotification;
 import client.maindisplay.SpotNumberDisplay;
 import client.maindisplay.WelcomeDisplay;
-// DV2 NEW TEST
+/*
+ * Test Purpose: 
+ * To test the call of getwDisp() from an EntranceDisplayController object and get coverage for the setter
+ * To test the call of getpDisp() from an EntranceDisplayController object and get coverage for the setter
+ * To test the call of getsDisp() from an EntranceDisplayController object and get coverage for the setter
+ * To test the call of getdDisp() from an EntranceDisplayController object and get coverage for the setter
+ */
 public class SST024_EntranceDisplayController_DisplayGettersSettersTest {
 	ControllerFacade facade;
 	WelcomeDisplay wDisp;
 	SpotNumberDisplay sDisp;
 	ParkingNotification pDisp;
 	DisplayDirections dDisp;
+	
+	/*
+	 * Test Setup:
+	 * Through a facade, instantiate an EntranceDisplayController object.
+	 * Inject a mock of WelcomeDisplay, Parkingnotification, SpotNumberDisplay
+	 * and DisplayDirections classes.
+	 */
 	@Before
 	public void setUp() throws Exception {
 		facade = new ControllerFacade();
@@ -38,24 +51,42 @@ public class SST024_EntranceDisplayController_DisplayGettersSettersTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	/*
+	 * Test Input: getwDisp() is called.
+	 * Expected Output: The object returned by getwDisp() has the same
+	 * memory address as the mocked object wDisp.
+	 */
 	@Test
-	public void testGetwDisp() {
+	public void SST024_EntranceDisplayController_testGetwDisp() {
 		assertEquals(facade.getEntranceDisplayController().getwDisp(), wDisp);
 	}
-
+	
+	/*
+	 * Test Input: getpDisp() is called.
+	 * Expected Outpuit: The object returned by getpDisp() has the same
+	 * memory address as the mocked object pDisp.
+	 */
 	@Test
-	public void testGetpDisp() {
+	public void SST031_EntranceDisplayController_testGetpDisp() {
 		assertEquals(facade.getEntranceDisplayController().getpDisp(), pDisp);
 	}
-
+	/*
+	 * Test Input: getsDisp() is called.
+	 * Expected Output: The object returned by getsDisp() has the same
+	 * memory address as the mocked object sDisp.
+	 */
 	@Test
-	public void testGetsDisp() {
+	public void SST032_EntranceDisplayController_testGetsDisp() {
 		assertEquals(facade.getEntranceDisplayController().getsDisp(), sDisp);
 	}
-
+	
+	/*
+	 * Test Input: getdDisp() is called
+	 * Expected Output: The object returned by getdDisp() has the same
+	 * memory address as the mocked object dDisp.
+	 */
 	@Test
-	public void testGetdDisp() {
+	public void SST033_EntranceDisplayController_testGetdDisp() {
 		assertEquals(facade.getEntranceDisplayController().getdDisp(), dDisp);
 	}
 
